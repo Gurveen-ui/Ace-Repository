7494675f3ab2423ea64a5689e50f90b642a38da7
Gurveen-ui <gurveen.kapoor.jk@gmail.com>
2025-12-07 22:24:11 +0000

    Added some more

--- Game Attempts/First Independant Try.py
+++ Game Attempts/First Independant Try.py
@@ -1,19 +1,33 @@
-import sys
 import pygame
+from sys import exit
 
 pygame.init()
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image
-        self.rect = self.image.get_rect()
-    pass
+#class Player(pygame.sprite.Sprite):
+#    def __init__(self):
+#        super().__init__()
+#        self.image
+#        self.rect = self.image.get_rect()
+#    pass
 
 Screen = pygame.display.set_mode((1800,1000))
+pygame.display.set_caption("Game")
+
+Test_Background = pygame.image.load("GitHub\\Ace-Repository\\Game Attempts\\Test Images\\Test Background.png").convert_alpha()
+test_surface = pygame.Surface((100,200))
+test_surface.fill("red")
+
+clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            quit()
+            exit()
+    print("Hi")
+    Screen.fill("white")
+    Screen.blit(test_surface, (0,0))
+    Screen.blit(Test_Background, (200,100))
+
+    pygame.display.update()
+    clock.tick(60)

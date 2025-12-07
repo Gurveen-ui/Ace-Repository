326b6ee0333768382e700cbae9f77cd6dc05713f
Gurveen-ui <gurveen.kapoor.jk@gmail.com>
2025-12-07 14:06:16 +0000

    Made Window

--- Game Attempts/First Independant Try.py
+++ Game Attempts/First Independant Try.py
@@ -1,2 +1,12 @@
 import sys
 import pygame
+
+pygame.init()
+Screen = pygame.display.set_mode((1800,1000))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+    

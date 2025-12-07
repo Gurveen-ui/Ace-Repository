3738eac05d58e04daf26ea67aa6bf322e6c9f11c
Gurveen-ui <gurveen.kapoor.jk@gmail.com>
2025-12-07 22:39:10 +0000

    Add files via upload

---
+++ Game Attempts/First Independant Try.py
@@ -0,0 +1,29 @@
+import pygame
+from sys import exit
+
+pygame.init()
+
+#class Player(pygame.sprite.Sprite):
+#    def __init__(self):
+#        super().__init__()
+#        self.image
+#        self.rect = self.image.get_rect()
+#    pass
+
+Screen = pygame.display.set_mode((1800,1000))
+pygame.display.set_caption("Game")
+
+Test_Background = pygame.image.load("D:\\Blaze\\Holiday learning\\Python\\Game Attempts\\Test Images\\Test Background.png").convert_alpha()
+
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    Screen.blit(Test_Background, (0,0))
+
+    pygame.display.update()
+    clock.tick(60)
